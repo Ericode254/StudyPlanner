8d003970c5f7cad89e87a2f65f2fab95b6b1f5fb
Ericode254 <147249996+Ericode254@users.noreply.github.com>
2024-01-22 19:03:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from kybra import query
 
 load_dotenv()
 
-openai.api_key = os.getenv("")
+openai.api_key = os.getenv("openai-api-key")
 
 app = Flask(__name__)
 CORS(app)
@@ -31,16 +31,6 @@ def frontend():
 @app.route('/study_plan_creator_frontend', methods=['POST', 'GET'])
 def study_plan_creator_frontend():
     return render_template('study_plan_creator_frontend.html')
-
-
-@app.route('/explain_concept_frontend', methods=['POST', 'GET'])
-def explain_concept_frontend():
-    return render_template('explain_concept_frontend.html')
-
-
-@app.route('/build_project_frontend', methods=['POST', 'GET'])
-def build_project_frontend():
-    return render_template('build_project_frontend.html')
 
 
 @app.route('/study_plan_creator', methods=['POST', 'GET'])
@@ -119,123 +109,6 @@ def study_plan_creator():
     return jsonify({"response": response.choices[0].message.content})
 
 
-@app.route('/explain_concept', methods=['POST', 'GET'])
-def explain_concept():
-    concept_name = request.form.get('concept_name')
-    weekly_time = request.form.get('weekly_time')
-    time_frame = request.form.get('time_frame')
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logger.info(f'Stated Time on explain concept: {time}')
-    logger.info(f'Concept Name: {concept_name}')
-
-    system_prompt = '''
-    You will be provided a topic.
-    You will provide an explanation on that given topic, assuming that the user has very little relevant knowledge.
-    Use analogies and examples in your explanation.
-    Also, include examples to implement the concept if applicable.
-    '''
-
-    query = f'''Explain "{concept_name}" in details.'''
-
-    try:
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            messages=[{
-                'role': 'system', 'content': system_prompt,
-                'role': 'user', 'content': query
-
-            }],
-            max_tokens=2000,
-            temperature=0.1
-        )
-    except Exception as e:
-        print(e)
-        logger.error(f'Error: {e}')
-        return jsonify({"error": e})
-
-    # print(response.choices[0].message.content)
-    logger.info(f'Response: {response.choices[0].message.content}')
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logger.info(f'End Time on explain concept: {time}')
-
-    return jsonify({"response": response.choices[0].message.content})
-
-
-@app.route('/build_project', methods=['POST', 'GET'])
-def build_project():
-    goal = request.form.get('goal')
-    project_type = request.form.get('project_type')
-    time_constraint = request.form.get('time_constraint')
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logger.info(f'Stated Time on build project: {time}')
-    logger.info(f'Goal: {goal}')
-    logger.info(f'Project Type: {project_type}')
-    logger.info(f'Time Constraint: {time_constraint}')
-
-    # Define the dictionary
-    # experts = {
-    # 'coding': 'Senior Programmer',
-    # 'art': 'Artist',
-    # 'art & craft': 'Craftsman',
-    # 'music': 'Musician',
-    # 'dance': 'Dancer',
-    # 'cooking': 'Chef',
-    # 'photography': 'Photographer',
-    # 'writing': 'Author',
-    # 'design': 'Designer',
-    # 'marketing': 'Marketing Specialist',
-    # 'finance': 'Financial Analyst',
-    # 'science': 'Scientist',
-    # 'mathematics': 'Mathematician',
-    # 'history': 'Historian',
-    # 'philosophy': 'Philosopher'
-    # }
-
-    experts = {
-        'General Coding': 'Senior Programmer',
-        'Data Science': 'Senior Data Scientist',
-        'AI Expert': 'AI Expert',
-        'Web Developement': 'Senior Web Developer'
-    }
-
-    expert = experts.get(project_type, 'General Coding Expert')
-
-    system_prompt = f'''
-    Act as an expert {expert}. User will give the description on a project.
-    Provide a project outline.
-    Also provide the starter codes for the project.
-    '''
-
-    query = f'''
-    {goal}. I have {time_constraint} to build this {project_type} project. Provide an outline {time_constraint.split()[-1].replace('s', '')}-by-{time_constraint.split()[-1].replace('s', '')}. 
-    Also provide the starter code afterwards.
-    '''
-
-    print(system_prompt, query)
-
-    try:
-        response = openai.ChatCompletion.create(
-            model='gpt-3.5-turbo',
-            # model= 'gpt-4',
-            messages=[{
-                'role': 'system', 'content': system_prompt,
-                'role': 'user', 'content': query
-
-            }],
-            max_tokens=2000,
-            temperature=0.1
-        )
-    except Exception as e:
-        print(e)
-        logger.error(f'Error: {e}')
-        return jsonify({"error": e})
-
-    # print(response.choices[0].message.content)
-    logger.info(f'Response: {response.choices[0].message.content}')
-    time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    logger.info(f'End Time on build project : {time}')
-
-    return jsonify({"response": response.choices[0].message.content})
 
     @query
     def display_info() -> str:
